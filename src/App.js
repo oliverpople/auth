2567f31aa8ab79a919b17d7486d68e0799d7e3dd
oliverpople <oliverpople@gmail.com>
2019-01-22 11:03:11 -0800

    Installed firebase library and connected to firebase via App.js.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React, { Component } from "react";
-import { View, Text } from "react-native";
-import { Header } from "./components/common";
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyB2zoO7t8FtWxr4CuXPyW_bb9abcibpWWc',
+      authDomain: 'authentication-ba61a.firebaseapp.com',
+      databaseURL: 'https://authentication-ba61a.firebaseio.com',
+      projectId: 'authentication-ba61a',
+      storageBucket: 'authentication-ba61a.appspot.com',
+      messagingSenderId: '968335474905'
+    });
+  }
+
   render() {
     return (
       <View>
